7930b2a21dca8ccb2c3cdc93af1caf92f8b650fd
AndresTabor <andrestaborda43@gmail.com>
2024-09-25 21:34:28 +0200

    solved exercise

---
+++ app.js
@@ -0,0 +1,83 @@
+/* 
+Funciones a elaborar:
+
+* animateByLine: 
+    Esta función debe imprimir el corazón línea por línea en la consola del editor, debe de haber un salto de línea entre cada impresión. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de línea.
+
+* animateByChart: 
+    Esta función debe imprimir el corazón carácter por carácter en la consola del editor, debe de haber un delay entre cada impresión de carácter. 
+    Cada vez que se complete la impresión de una línea, se debe hacer un salto de línea. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de carácter.
+
+Pistas:
+
+- Investiga sobre la función setTimeout, puede ser de interés.
+- Usa console.log o process.stdout.write para imprimir en la consola de acuerdo a lo que necesites.
+- Recuerdas que necesitas iterar un string, ¿cómo lo conviertes en un array de caracteres?
+
+*/
+
+/*
+ Ejecutar el comando <' node app.js '> para ver el resultado del codigo
+
+*/
+
+
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+]
+
+
+
+const animateByChart = (ms) => {
+    let delay = ms;
+    asciiHeart.map(line => { 
+        let charsArray = line.split("");
+
+        /*charsArray.map(char =>{
+            setTimeout(() => {
+                process.stdout.write(char);
+                if (charsArray.lastIndexOf(char) === charsArray.length - 1) {
+                    process.stdout.write('\n');
+                }
+            }, delay); 
+            delay += ms
+                     
+        })*/
+
+        for (let index = 0; index < charsArray.length; index++) {
+            setTimeout(() => {
+                process.stdout.write(charsArray[index]);
+                if (index === charsArray.length - 1) {
+                    process.stdout.write('\n');
+                }
+            }, delay); 
+            delay += ms
+            
+        }
+        
+    })
+}
+
+
+const animateByLine = (ms) => {
+    let delay = ms;
+    asciiHeart.map(line => {
+        setTimeout(() => {
+            console.log(line);
+        }, delay);
+        delay += ms
+    })
+}
+
+animateByChart(70)
+//animateByLine(2000)
